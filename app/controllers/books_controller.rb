2ded571cc70c35c9029a1290637bfb2a5ae61c78
EC2 Default User <ec2-user@ip-172-31-7-224.ec2.internal>
2022-03-07 13:24:08 +0000

    [Finish] 3-課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,53 @@
 class BooksController < ApplicationController
-  def new
+  # def new
+  #   @book=Book.new
+  # end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:complete] = "Book was successfully created."
+      # TODO: ＠bookの@がいるのはなぜ？
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+   @book= Book.find(params[:id])
+   if @book.update(book_params)
+     flash[:complete] = "Book was successfully updated."
+    # TODO: bookに＠がつかないのはなぜ？
+   redirect_to book_path(@book.id)
+   else
+     render :edit
+   end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to'/books'
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
